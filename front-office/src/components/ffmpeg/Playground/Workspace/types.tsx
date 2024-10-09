e361794a75aec6d01e2171dcea42373cca80201e
veepdotai <jck@veep.ai>
2024-10-09 15:00:23 +0000

    Minor correction

--- front-office/src/components/ffmpeg/Playground/Workspace/types.tsx
+++
@@ -1,4 +0,0 @@
-export interface Node {
-  name: string;
-  isDir: boolean;
-}
--- front-office/src/components/ffmpeg/util.ts
+++
@@ -1,9 +0,0 @@
-export const downloadFile = (name: string, data: ArrayBuffer | string) => {
-  const a = document.createElement("a");
-  const blob = new Blob([data]);
-  const url = window.URL.createObjectURL(blob);
-  a.href = url;
-  a.download = name;
-  a.click();
-  window.URL.revokeObjectURL(url);
-};
